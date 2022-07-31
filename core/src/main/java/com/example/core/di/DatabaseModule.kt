850113cf591abd6e90832f6e39992faf2ec8cbad
Antika20 <antikaorin20@gmail.com>
2022-07-31 22:27:33 +0700

    ADD Leak Canary , CI , Enkripsi, obfuscation ,  certificate pinning

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -9,18 +9,29 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn (SingletonComponent::class)
 object DatabaseModule {
+
+    val passphrase: ByteArray = SQLiteDatabase.getBytes("example".toCharArray())
+    val factory = SupportFactory(passphrase)
+
     @Singleton
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): MovieDatabase =
+
+
         Room.databaseBuilder(
             context,
             MovieDatabase::class.java,"Movie.db"
-        ).fallbackToDestructiveMigration().allowMainThreadQueries().build()
+        ).fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
+            .openHelperFactory(factory)
+            .build()
 
     @Provides
     fun provideMovieDao(database: MovieDatabase): MovieDao = database.movieDao()
